cf83e1d85dda89961a3939f02d47f14bd1c6dbce
paulwainaina <paulwainaina36@gmail.com>
2021-11-18 11:50:18 +0300

    notfound page

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"io/ioutil"
 	"log"
@@ -9,19 +10,20 @@ import (
 
 var tpl *template.Template
 
-func init(){
-	tpl= template.Must(template.ParseGlob("templates/*.gohtml"))
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
 }
 
 type Page struct {
 	Title string
-	Body  []byte
+	Body  []byte	
 }
 
 func loadPage(title string) (*Page, error) {
-	filename := title + ".gohtml"
+	filename := "templates/" + title + ".gohtml"
 	body, err := ioutil.ReadFile(filename)
 	if err != nil {
+		fmt.Println(err.Error())
 		return nil, err
 	}
 	return &Page{Title: title, Body: body}, nil
@@ -36,7 +38,29 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Redirect(w, r, "/notFound", http.StatusFound)
+	}
 	title := "index"
+	page, err := loadPage(title)
+	if err != nil {
+		page = &Page{Title: title}
+	}
+	renderTemplate(w, title, page)
+}
+
+func hideWindowHandler(w http.ResponseWriter, r *http.Request) {
+	title := "hideWindow"
+	page, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/notFound", http.StatusFound)
+		return
+	}
+	renderTemplate(w, title, page)
+}
+
+func notFoundPage(w http.ResponseWriter, r *http.Request) {
+	title := "404"
 	page, err := loadPage(title)
 	if err != nil {
 		page = &Page{Title: title}
@@ -47,5 +71,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/hideWindow", hideWindowHandler)
+	http.HandleFunc("/notFound", notFoundPage)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
